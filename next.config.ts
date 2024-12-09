337e1e958de824cf78563311d008a97377a4fb31
vikasazad <vikumar.azad@gmail.com>
2024-12-09 14:52:27 +0530

    09/12/2024

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
-import withPWAInit from "@ducanh2912/next-pwa";
 
-const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: false,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-const nextConfig = {
+const nextConfig: import("next").NextConfig = {
   reactStrictMode: true,
   compress: true,
   images: {
@@ -25,4 +13,4 @@ const nextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
